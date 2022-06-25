8e6cf769973932337e2a284c829188e63226ed5e
marcosarcu <marcosarcu@gmail.com>
2022-06-24 21:41:48 -0300

    Update carritoView.vue

--- sitio/app/src/views/carritoView.vue
+++ sitio/app/src/views/carritoView.vue
@@ -1,25 +1,24 @@
 <template>
 
   <main class="container">
-
-    <table class="table table-striped mt-5">
+    <table class="table table-striped mt-5" v-if="carrito.length>0">
 
       <thead>
         <tr>
-          <th scope="col">#</th>
-          <th scope="col">Precio</th>
           <th scope="col">Productos</th>
+          <th scope="col">Precio</th>
           <th scope="col">Cantidad</th>
+          <th scope="col">Acciones</th>
         </tr>
       </thead>
 
       <tbody>
 
         <!-- producto :key="vinilo.id -->
-        <tr v-for="(vinilo, index) in carrito">
+        <tr v-for="(vinilo, index) in carrito" :key="vinilo.id">
           
           <!-- Id -->
-          <td class="row">{{ index }}</td>
+          <!-- <td class="row">{{ index }}</td> -->
 
           <!-- Img y Titulo -->
           <td class="table__productos">
@@ -33,7 +32,7 @@
 
           <!-- Precio -->
           <td class="table__precio">
-            <p>${{vinilo.precio}}</p>
+            <p>${{vinilo.precio.toLocaleString("ES-AR")}}</p>
           </td>
 
           <!-- Cantidad -->
@@ -52,7 +51,8 @@
             <button
               @click="$emit('eliminar', index)"
               class="delete btn btn-danger">
-              x
+              <i class="fa-solid fa-trash margin-right"></i>
+              <span>Eliminar</span>
             </button>
 
           </td>
@@ -63,15 +63,22 @@
 
     </table>
 
-    <div class="row mx-4 mb-5">
+    <div class="row mx-4 mb-5" v-if="carrito.length>0">
       <div class="col">
         <h3 class="itemCardTotal">Total: $ <span v-text="total"></span></h3>
         <h3 class="itemCardTotal">Cantidad: <span v-text="cantidad"></span></h3>
       </div>
 
-      <div class="col d-flex justify-content-end">
-        <button class="btn btn-success">COMPRAR</button>
+      <div class="col d-flex justify-content-end" v-if="carrito.length>0">
+        <button class="btn btn-success button">Comprar</button>
       </div>
+    </div>
+
+    <div v-if="!(carrito.length>0)">
+      <h3 class="text-center p-5">No hay productos en el carrito</h3>
+      <router-link class="btn btn-success button mb-5 mx-auto d-block width-fit" to="/catalogo">
+        Ver catálogo
+      </router-link>
     </div>
 
   </main>
@@ -92,11 +99,9 @@
 
     },
 
-    computed(){
-/*       this.carrito = localStorage.getItem('vinilos') ? JSON.parse(localStorage.getItem('vinilos')) : [];
-      this.total = localStorage.getItem('total') ? JSON.parse(localStorage.getItem('total')) : [];
-      this.cantidad = localStorage.getItem('cantidad') ? JSON.parse(localStorage.getItem('cantidad')) : []; */
-    },
+    // computed: {
+
+    // },
 
   };
 
@@ -108,6 +113,9 @@
   max-width: 1000px;
   margin: 0 auto;
   width: 100%;
+}
+.margin-right{
+  margin-right: 10px;
 }
 .table__productos {
   display: flex;
@@ -127,4 +135,15 @@
   margin-right: 30px;
   margin-bottom: 20px;
 }
+
+td{
+  align-items: center;
+  vertical-align: middle;
+}
+
+.width-fit{
+  width: fit-content
+}
+
+
 </style>
